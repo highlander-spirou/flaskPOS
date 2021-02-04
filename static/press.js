58b0b138b575c5f6b1944c35d0e1eb66214eeff3
shynwsl <highlandetspirou2812@gmail.com>
2021-02-04 19:06:20 +0700

    dynamic query

--- static/press.js
+++ static/press.js
@@ -1,17 +1,36 @@
+const deepFind = (f, obj = {}) =>
+{ if (Object (obj) === obj)
+  { if (f (obj) === true)
+      return obj
+
+    for (const [ k, v ] of Object.entries (obj))
+    { const res =
+        deepFind (f, v)
+
+      if (res !== undefined)
+        return res
+    }
+  }
+
+  return undefined
+}
+
+
 
 window.onload = function () {
-  document.getElementById("btn").addEventListener("click", getText);
+  document.getElementById("input1").addEventListener("input", getText);
 };
-
 
 function getText() {
   var c = document.getElementById("input1").value;
   fetch("/tabledata")
     .then((res) => res.json())
     .then((data) => {
-        const d = deepFind (x => x.name === "asdj", data)
-        console.log(d)
-    })
-
+      const d = deepFind((x) => x.name === c, data);
+      if (typeof d !== "undefined") {
+        document.getElementById("input2").value = d.company;
+      } else {
+        document.getElementById("input2").value = "";
+      }
+    });
 }
-
